cb21b781ac347ccdf70eea676628c69f5904fe30
LogoCat <kuohsuanlo@gmail.com>
2018-05-21 01:41:43 +0800

    add more exception detection.

--- src/main/java/io/github/kuohsuanlo/coinhivemc/util/CoinHiveWebUtil.java
+++ src/main/java/io/github/kuohsuanlo/coinhivemc/util/CoinHiveWebUtil.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.Scanner;
@@ -128,6 +129,8 @@ public class CoinHiveWebUtil {
 	    } catch (MalformedURLException e) {
 			e.printStackTrace();
 	    } catch (SocketTimeoutException e) {
+			e.printStackTrace();
+	    } catch (SocketException e) {
 			e.printStackTrace();
 	    } catch (SSLException e) {
 			e.printStackTrace();
